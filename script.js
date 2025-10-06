da0bab08d4852d82eca4299f6ebece35c8796485
godpromise <akuevshi@gmail.com>
2025-10-06 22:35:03 +0500

    somechangesidk

--- script.js
+++ script.js
@@ -1,45 +1,48 @@
+const BtnSearch = document.getElementById('ButtonSearch')
+const list = document.getElementById('list')
 const tags = ['Все', 'Идеи', 'Личное', 'Работа']
 const notes = [
     {
     title: 'Сдать отчёт',
     tag: 'Работа',
-    uptadeAt: new Date()
+    updateAt: new Date()
 },
     {
     title: 'Сдать отчёт',
     tag: 'Работа',
-    uptadeAt: new Date()
+    updateAt: new Date()
 },
 ]
 
-function createNote(notes) {
+function createNote(note) {
     const element = document.createElement('div')
     element.classList.add('NoteBoxItem')
     
     const title = document.createElement('span')
-    title.innerHTML = '1'
+    title.innerText = note.title
     title.classList.add('NoteTitle')
     
     const date = document.createElement('span')
     date.classList.add('NoteData')
+    date.innerText = note.updateAt
 
     const tag = document.createElement('span')
+    tag.innerText = note.tag
     tag.classList.add('NoteTag')
 
     element.appendChild(title)
     element.appendChild(date)
     element.appendChild(tag)
+
     return element
 }
 
-
 function render() { 
-    list.innerHTML= ''
-    for (let i of notes) {
-        const element = createNote(i)
-        list.appendChild(element)
-    }
+    list.innerHTML = ''
+    createNote(notes)
 }
+
+
 
 function init() {
     render()
