56d914ec4df87a4ef6531e6fed988e364bcba071
Голиков Богдан Артурович <golikov_ba@edu.surgu.ru>
2025-10-09 12:53:56 +0500

    bigChanges

--- script.js
+++ script.js
@@ -1,80 +1,33 @@
 // const BtnSearch = document.getElementById('ButtonSearch')
-// const list = document.getElementById('list')
+const list = document.getElementById('list')
 // const tags = ['Все', 'Идеи', 'Личное', 'Работа']
-// const notes = [
-//     {
-//     title: 'Сдать отчёт',
-//     tag: 'Работа',
-//     updateAt: new Date()
-// },
-//     {
-//     title: 'Сдать отчёт',
-//     tag: 'Работа',
-//     updateAt: new Date()
-// },
-// ]
-
-// function createNote(note) {
-//     const element = document.createElement('div')
-//     element.classList.add('NoteBoxItem')
-    
-//     const title = document.createElement('span')
-//     title.innerText = note.title
-//     title.classList.add('NoteTitle')
-    
-//     const date = document.createElement('span')
-//     date.classList.add('NoteData')
-//     date.innerText = note.updateAt
-
-//     const tag = document.createElement('span')
-//     tag.innerText = note.tag
-//     tag.classList.add('NoteTag')
-
-//     element.appendChild(title)
-//     element.appendChild(date)
-//     element.appendChild(tag)
-
-//     return element
-// }
-
-// function render() { 
-//     list.innerHTML = ''
-//     createNote(notes)
-// }
-
-
-
-// function init() {
-//     render()
-// }
-
-// init()
-
-
-// const BtnSearch = document.getElementById('ButtonSearch')
+const BtnSearch = document.getElementById('ButtonSearch')
+const SearchText = document.getElementById('searchText')
 // const list = document.getElementById('list')
+let activeTag = 1
 const tags = ['Все', 'Идеи', 'Личное', 'Работа']
 const notes = [
     {
-    title: 'Сдать отчёт',
-    tag: 'Работа',
-    updateAt: new Date()
-},
+        title: 'Сдать отчёт',
+        tag: 'Работа',
+        updateAt: new Date()
+    },
     {
-    title: 'Сдать отчёт',
-    tag: 'Работа',
-    updateAt: new Date()
-},
+        title: 'ASDFСдать отчётadf',
+        tag: 'Работа',
+        updateAt: new Date()
+    },
 ]
 
 function createNote(note) {
+
     const element = document.createElement('div')
     element.classList.add('NoteBoxItem')
-    
+
     const title = document.createElement('span')
     title.innerText = note.title
     title.classList.add('NoteTitle')
-    
+
     const date = document.createElement('span')
     date.classList.add('NoteData')
     date.innerText = note.updateAt
@@ -90,13 +43,29 @@ function createNote(note) {
     return element
 }
 
-function render() { 
-    list.innerHTML = ''
-    createNote()
+
+
+function NotesFilter() {
+    return notes.filter((e) => e.title.startsWith(SearchText.value))
+}
+
+function render() {
+    list.innerHTML = ' '
+    let renderArr = notes
+    if (SearchText.value.length) {
+        renderArr = NotesFilter()
+    }
+    for (const note of renderArr) {
+        const zametka = createNote(note)
+        list.appendChild(zametka)
+    }
+
+
 }
 
 function init() {
     render()
+    BtnSearch.addEventListener('click', render)
 }
 
 init()
